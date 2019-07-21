c76e5622085c3e67e6e234bc80d578aedddc3d74
WeirdVal <valerene_goh@mymail.sutd.edu.sg>
2019-07-21 08:38:52 +0800

    commit

--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -10,34 +10,6 @@ using Proyecto26;     // for Firebase
 
 public class DropZone : MonoBehaviour, IDropHandler{
 
-    public string DeviceName = "HMSoft";
-    public string ServiceUUID = "FFE0";
-    public string Characteristic = "FFE1";
-    private string _hm10;
-
-     enum States{
-        None,
-        Scan,
-        Connect,
-        Subscribe,
-        Unsubscribe,
-        Disconnect,
-        Communication,
-    }
-
-    private bool _workingFoundDevice = true;
-    private bool _connected = false;
-    private float _timeout = 0f;
-    private States _state = States.None;
-
-    void Reset (){
-        _workingFoundDevice = false;    // used to guard against trying to connect to a second device while still connecting to the first
-        _connected = false;
-        _timeout = 0f;
-        _state = States.None;
-        _hm10 = null;
-    }
-
     public Dictionary<int, string> dict = new Dictionary<int, string>(){
             {48, "0"},  // C
             {49, "1"},  // C#
@@ -71,23 +43,14 @@ public class DropZone : MonoBehaviour, IDropHandler{
     public Draggable d;
     public GameObject scroll;
     private bool canRotatePlayer = false;
-    public SerialPort stream;
-    public Text HM10_Status;
-    public bool enabled;
+    // public SerialPort stream;
 
     void Start(){
-        HM10_Status.text = "Initializing...";
-        Reset();
-        BluetoothLEHardwareInterface.Initialize (true, false, () => {
-            SetState (States.Scan, 0.1f);
-            HM10_Status.text = "Initialized";
-        }, (error) => {
-            BluetoothLEHardwareInterface.Log ("Error: " + error);
-        });
-        enabled = false;
+        InitializeBluetooth();
         StartCoroutine(ShowStatistics());
     }
 
+    // What happens when user drags and drops a disc onto the vinyl plate.
     public void OnDrop(PointerEventData eventData){
         d = eventData.pointerDrag.GetComponent<Draggable>();
         if(d != null){
@@ -106,6 +69,116 @@ public class DropZone : MonoBehaviour, IDropHandler{
 
     void Update(){
         RotatePlayer();
+        ConnectBluetooth();
+    }
+
+    // What happens when song playing is done.
+    private IEnumerator ReturnPlayer(float waitTime){
+        yield return new WaitForSeconds(waitTime);  //wait awhile before returning disc to slot
+        canRotatePlayer = false;
+        scroll.GetComponent<ScrollRect>().enabled = true;
+        d.transform.SetParent(d.parent);
+        d.parentToReturnTo = d.parent;
+        d = null;
+        foreach (Draggable cd in CdList){
+            if (cd.name != title){
+                cd.draggable = true;
+            }
+        }
+    }
+
+    // What happens during song play.
+    private IEnumerator SendArduino(){
+        d.RetrieveFromDatabase();
+        yield return new WaitForSeconds(1.0f);  //wait awhile before getting popularity score & playing song
+        print("Playing Track: " + d.getTitle() + " of popularity " + d.getPopularity());
+        Song song = new Song();
+        song.setPopularity(d.getPopularity() + 1);        // update view count
+        song.setTitle(d.getTitle());
+        RestClient.Put("https://pico-86a8b.firebaseio.com/" + d.getTitle() + ".json", song);
+
+        WaitForSeconds wait = new WaitForSeconds(0.5f);
+        string[] lines = d.TextFile.text.Split('\n');
+        foreach (string line in lines){
+            if(!string.IsNullOrWhiteSpace(line)){    // beat contains notes
+                foreach(string note in Regex.Split(line, " ")){
+                    int key = int.Parse(note);
+                    // stream.Write(dict[key]);
+                    var data = Encoding.UTF8.GetBytes (dict[key]);
+                    BluetoothLEHardwareInterface.WriteCharacteristic (_hm10, ServiceUUID, Characteristic, data, data.Length, false, (characteristicUUID) => {
+                        BluetoothLEHardwareInterface.Log ("Write Succeeded");
+                    });
+                }   
+            }
+            yield return wait; //tempo of song
+        }
+        StartCoroutine(ReturnPlayer(1.0f));
+    }
+
+    protected void RotatePlayer(){
+        if(canRotatePlayer){
+            d.transform.Rotate(Vector3.forward, -90.0f * Time.deltaTime);
+        }
+    }
+
+    // =========================== SETTINGS SCREEN =========================== 
+
+    public Draggable TestFile;
+    public Text statistics;
+    public Dictionary<string, string> playlist = new Dictionary<string, string>();
+
+    public void TestScale(){
+        d = TestFile;
+        StartCoroutine(SendArduino());
+    }
+
+    public IEnumerator ShowStatistics(){
+        foreach(Draggable cd in CdList){
+            Song song = new Song();
+            RestClient.Get<Song>("https://pico-86a8b.firebaseio.com/" + cd.getTitle() + ".json").Then(response =>{
+                    song = response;
+                    playlist[cd.getTitle()] = song.getPopularity().ToString();   //update values
+                });
+        }
+        yield return new WaitForSeconds(1.0f);  //wait awhile before updating statistics
+        statistics.text = "";
+        int count = 0;
+        foreach(KeyValuePair<string, string> pair in playlist){
+            count += 1;
+            statistics.text += string.Format("{0}.\t{1}: {2}\n", count, pair.Key, pair.Value);
+        }
+    }
+
+    // =========================== BLUETOOTH FUNCTIONS =========================== 
+
+    public Text HM10_Status;
+    public string DeviceName = "HMSoft";
+    public string ServiceUUID = "FFE0";
+    public string Characteristic = "FFE1";
+    private string _hm10;
+
+     enum States{
+        None,
+        Scan,
+        Connect,
+        Subscribe,
+        Unsubscribe,
+        Disconnect,
+        Communication,
+    }
+
+    public void InitializeBluetooth(){
+        HM10_Status.text = "Initializing...";
+        Reset();
+        BluetoothLEHardwareInterface.Initialize (true, false, () => {
+            SetState (States.Scan, 0.1f);
+            HM10_Status.text = "Initialized";
+        }, (error) => {
+            BluetoothLEHardwareInterface.Log ("Error: " + error);
+        });
+    }
+
+    public void ConnectBluetooth(){
         if (_timeout > 0f){
             _timeout -= Time.deltaTime;
             if (_timeout <= 0f){
@@ -176,53 +249,19 @@ public class DropZone : MonoBehaviour, IDropHandler{
                 }
             }
         }
-     }
-
-    private IEnumerator ReturnPlayer(float waitTime){
-        yield return new WaitForSeconds(waitTime);
-        canRotatePlayer = false;
-        scroll.GetComponent<ScrollRect>().enabled = true;
-        d.transform.SetParent(d.parent);
-        d.parentToReturnTo = d.parent;
-        d = null;
-        foreach (Draggable cd in CdList){
-            if (cd.name != title){
-                cd.draggable = true;
-            }
-        }
     }
 
-    private IEnumerator SendArduino(){
-        d.RetrieveFromDatabase();
-        yield return new WaitForSeconds(1.0f);  //wait awhile before getting popularity score
-        print("Playing Track: " + d.getTitle() + " of popularity " + d.getPopularity());
-        Song song = new Song();
-        song.setPopularity(d.getPopularity() + 1);        // update view count
-        song.setTitle(d.getTitle());
-        RestClient.Put("https://pico-86a8b.firebaseio.com/" + d.getTitle() + ".json", song);
+    private bool _workingFoundDevice = true;
+    private bool _connected = false;
+    private float _timeout = 0f;
+    private States _state = States.None;
 
-        WaitForSeconds wait = new WaitForSeconds(0.5f);
-        string[] lines = d.TextFile.text.Split('\n');
-        foreach (string line in lines){
-            if(!string.IsNullOrWhiteSpace(line)){    // beat contains notes
-                foreach(string note in Regex.Split(line, " ")){
-                    int key = int.Parse(note);
-                    // stream.Write(dict[key]);
-                    var data = Encoding.UTF8.GetBytes (dict[key]);
-                    BluetoothLEHardwareInterface.WriteCharacteristic (_hm10, ServiceUUID, Characteristic, data, data.Length, false, (characteristicUUID) => {
-                        BluetoothLEHardwareInterface.Log ("Write Succeeded");
-                    });
-                }   
-            }
-            yield return wait; //tempo of song
-        }
-        StartCoroutine(ReturnPlayer(1.0f));
-    }
-
-    protected void RotatePlayer(){
-        if(canRotatePlayer){
-            d.transform.Rotate(Vector3.forward, -90.0f * Time.deltaTime);
-        }
+    void Reset (){
+        _workingFoundDevice = false;    // used to guard against trying to connect to a second device while still connecting to the first
+        _connected = false;
+        _timeout = 0f;
+        _state = States.None;
+        _hm10 = null;
     }
     
     bool IsEqual(string uuid1, string uuid2){
@@ -241,33 +280,5 @@ public class DropZone : MonoBehaviour, IDropHandler{
     void SetState (States newState, float timeout){
         _state = newState;
         _timeout = timeout;
-    }
-
-    // SETTINGS SCREEN
-
-    public Draggable TestFile;
-    public Text statistics;
-    public Dictionary<string, string> playlist = new Dictionary<string, string>();
-
-    public void TestScale(){
-        d = TestFile;
-        StartCoroutine(SendArduino());
-    }
-
-    public IEnumerator ShowStatistics(){
-        foreach(Draggable cd in CdList){
-            Song song = new Song();
-            RestClient.Get<Song>("https://pico-86a8b.firebaseio.com/" + cd.getTitle() + ".json").Then(response =>{
-                    song = response;
-                    playlist[cd.getTitle()] = song.getPopularity().ToString();   //update values
-                });
-        }
-        yield return new WaitForSeconds(1.0f);  //wait awhile before updating statistics
-        statistics.text = "";
-        int count = 0;
-        foreach(KeyValuePair<string, string> pair in playlist){
-            count += 1;
-            statistics.text += string.Format("{0}.\t{1}: {2}\n", count, pair.Key, pair.Value);
-        }
     }
 }
